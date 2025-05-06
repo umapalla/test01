701439117340c578095e799d25fc4bb54ad18c5b
uma palla <pumarao57@gmail.com>
2025-05-06 14:13:05 +1000

    git steps to push file to remote repo

---
+++ git_commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "To uplod script from local repo to remote repo"
+
+echo " git add <filename>; git status (to check which files are currently in staging area)"
+
+echo " git commit -m "message" (moving the file from staging area to local repo)"
+
+echo " git push origin main  (moving file from local repo to remote repo)"
