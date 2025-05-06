a16e225bea18aaa86ccb4d5c2c580a5436c2f0c7
uma palla <pumarao57@gmail.com>
2025-05-06 16:44:49 +1000

    test

--- 05_arrays.sh
+++ 05_arrays.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-fruits=("apple", "mango", "orange")
+fruits=("apple" "mango" "orange")
 
 echo "first fruit is: ${fruits[0]}"
 echo "second fruit is: ${fruits[1]}"
 echo "third fruit is: ${fruits[2]}"
+echo "${fruits[@]}"
---
+++ 06_command_output.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+DATE=$(date)
+
+echo ("today date is $DATE")
